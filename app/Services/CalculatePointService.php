92cce173cc03ecb9ef6ab1dff5c8f5d04ba61cc2
tsuyoshi-motoyama <motoyama@fix-japan.work>
2021-09-26 00:14:39 +0900

    test 9-1-5

---
+++ app/Services/CalculatePointService.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Exceptions\PreconditionException;
+
+final class CalculatePointService
+{
+    /**
+     * @param int $amount
+     * @return int
+     * @throws PreconditionException
+     */
+    public static function calcPoint(int $amount): int
+    {
+        if ($amount < 0) {
+            throw new PreconditionException('購入金額がマイナス');
+        }
+
+        if ($amount < 1000) {
+            return 0;
+        }
+
+        if ($amount < 10000) {
+            $basePoint = 1;
+        } else {
+            $basePoint = 2;
+        }
+
+        return intval($amount / 100) * $basePoint;
+    }
+}
